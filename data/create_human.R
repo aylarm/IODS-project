b6cdf3c2c5f4253d1e50ee418c273de813301745
aylarm <aylar.motan@gmail.com>
2017-11-29 22:00:05 +0200

    exercise 4

---
+++ data/create_human.R
@@ -0,0 +1,37 @@
+hd <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human_development.csv", stringsAsFactors = F)
+gii <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/gender_inequality.csv", stringsAsFactors = F, na.strings = "..")
+str(hd)
+dim(hd)
+str(gii)
+dim(gii)
+summary(hd)
+summary(gii)
+options(StringsAsFactors=FALSE)
+gii_cols = c(
+  "hdi_rank", "country", "GII_2012", "maternal_mortality_ratio",
+  "adolescent_fertility_rate_gii", "seats_parlim_female_gii",
+  "seats_parlim_female_note", "secondary_education_female",
+  "secondary_education_female_note", "secondary_education_male",
+  "secondary_education_male_note", "labor_participation_female_gii",
+  "labor_participation_male_gii"
+)
+df_all = tbl_df(read.csv('http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/gender_inequality.csv"', col.names=gii_cols,
+                         na.strings='..'))
+df_all = df_all %>% select(-hdi_rank, -seats_parlim_female_note,
+                           -secondary_education_female_note,
+                           -secondary_education_male_note)
+ inner_join(hd,gii)
+ rownames(df_all) = df_all$Country
+ region_info = read.delim('http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human_development.csv')
+ region_names = read.csv('http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/gender_inequality.csv')
+ region_info$region = region_names$Region[match(region_info$region.code,
+                                                region_names$M49.numeric.code)]
+ regions = region_info$region[match(df_all$Country, region_info$name)]
+ country_names = as.character(df_all$Country)
+ df_all = df_all %>% select(-Country)
+ 
+ print("Number of missing df_allapoints for each variable:")
+ apply(df_all, 2, function (x) {sum(is.na(x))})
+ print("Countries with missing data:")
+ country_names[!complete.cases(df_all)]
+ human <-df_all
